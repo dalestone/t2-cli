3fa4087363b137e0330a85cf3499683f95e4f397
johnnyman727 <johnnyman727@gmail.com>
2015-06-27 14:23:50 -0700

    First go at preferring LAN connections

--- test/unit/provision.js
+++ test/unit/provision.js
@@ -97,7 +97,7 @@ exports['controller.provisionTessel'] = {
   },
 
   tearDown: function(done) {
-    this.tessel.close();
+    this.tessel.mockClose();
     this.tessel._rps.removeAllListeners();
     this.isProvisioned.restore();
     this.provisionTessel.restore();
@@ -187,7 +187,7 @@ exports['Tessel.prototype.provision'] = {
   },
 
   tearDown: function(done) {
-    this.tessel.close();
+    this.tessel.mockClose();
     this.provision.restore();
     this.setupLocal.restore();
     this.writeFileSpy.restore();
